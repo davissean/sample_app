7a3e196de7fdccbea89aaf08fbe2158ab2cb36b8
davissean <sdavis1@crimson.ua.edu>
2012-07-02 23:45:01 -0500

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,9 +9,9 @@ end
 
 def sign_in(user)
 	visit signin_path
-	fill_in "Email", 	with: user.Email
-	fill_in "Password",	with: user.Password
+	fill_in "Email", 	with: user.email
+	fill_in "Password",	with: user.password
 	click_button "Sign in"
-	# Sign in when not using capybar as well.
+	# Sign in when not using capybara as well.
 	cookies[:remember_token] = user.remember_token
 end
